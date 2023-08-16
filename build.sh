3e0fad7446a6751a0f5876c116bf47ac36bad8e4
Joey Pekar <joey.pekar@protonmail.com>
2023-08-16 13:56:07 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit # exit on error
 
 poetry install
 
+python manage.py tailwind install
 python manage.py tailwind build
 python manage.py collectstatic --no-input
 python manage.py migrate
